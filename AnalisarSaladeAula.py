77d72f1b9648a7dc6c62c64e90290872138a0a4d
Celso Araujo <celsopa@msn.com>
2019-06-14 17:16:33 -0300

    Add two files. The main code and the modules.
    funcoesProjeto.py -> module with function that helps to create a random database file and analyze the classroom.
    AnalisarSaladeAula.py -> main code with a simple menu to create database, list files at the directory and analyze the classroom.

---
+++ AnalisarSaladeAula.py
@@ -0,0 +1,78 @@
+from Projeto_FinalCurso.funcoesProjeto import *
+from os import listdir, system
+
+while True:
+    try:
+        print(f"""O que deseja fazer?
+        [ 1 ] Gerar informações da turma.
+        [ 2 ] Exibir arquivos disponíveis.
+        [ 3 ] Analisar arquivo da turma.
+        [ 4 ] SAIR""")
+        acao = int(input())
+
+        # COMANDO GERAR BASE DE DADOS
+        if acao == 1:
+            nome = input("Informe o nome do arquivo a ser criado: ").strip()
+            while nome == "":
+                nome = input("Informe o nome do arquivo a ser criado: ").strip()
+            gerarturma(nome)
+
+        # COMANDO EXIBIR ARQUIVOS .TXT
+        elif acao == 2:
+            items = listdir("./")
+            arquivos_txt = []
+            for item in items:
+                if item.endswith(".txt"):
+                    arquivos_txt.append(item)
+            if len(arquivos_txt):
+                print("-=-" * 30)
+                print(f"Lista de arquivos encontrados: ", end=" ")
+                for x in range(len(arquivos_txt)):
+                    if x == len(arquivos_txt) - 1:
+                        print(arquivos_txt[x])
+                    else:
+                        print(arquivos_txt[x], end=" | ")
+                print("-=-" * 30)
+            else:
+                print("Não foram encontrados arquivos .TXT")
+
+        # COMANDO ANALISAR BASE DE DADOS
+        elif acao == 3:
+            while True:
+                try:
+                    arquivo_entrada = input("Informe o arquivo a ser analisado [com formato]: ").strip().lower()
+                    arquivo_entrada = open(arquivo_entrada, encoding="UTF-8")
+                    break
+                except:
+                    print("Arquivo não localizado.")
+
+            arquivo_saida = input(
+                "Informe o nome do arquivo onde serão salvas as informações [sem formato]: ").strip().lower()
+            while True:
+                try:
+                    print("""Informe o formato do arquivo que deverá ser criado:
+                    [ 1 ] Formato [.txt]
+                    [ 2 ] Formato [.html]""")
+                    formato_saida = int(input())
+                    if formato_saida == 1:
+                        formato_saida = 'txt'
+                        break
+                    elif formato_saida == 2:
+                        formato_saida = 'html'
+                        break
+                    else:
+                        print("Formato inválido.")
+                except:
+                    print("Formato inválido.")
+                    continue
+            analisarturma(arquivo_saida, formato_saida, arquivo_entrada)
+
+        # COMANDO SAIR
+        elif acao == 4:
+            print("Aplicação encerrada.")
+            system("PAUSE")
+            break
+        else:
+            print("Ação inválida.")
+    except:
+        continue
---
+++ funcoesProjeto.py
@@ -0,0 +1,126 @@
+from random import random, randint
+
+notasTurma = []
+situacaoTurma = []
+
+
+def analisarturma(novo_arquivo, formato, base_dados):
+    """
+    Analisa um arquivo com dados dos alunos de uma classe e retorna uma tabela com a situação de cada um.
+    :param novo_arquivo: Nome do novo arquivo onde serão salvas as informações da turma.
+    :param formato: Formato do arquivo que será criado.
+    :param base_dados: Nome do arquivo com indicação do formato onde constam os dados que serão analisados.
+    :return: Um arquivo formatado contendo a situação de cada aluno informado.
+    """
+    arquivo = base_dados
+    # arquivo = open(base_dados)
+    conteudo = []
+
+    for line in arquivo:
+        linhas = line.split('|')
+        nome = linhas[0].strip()
+        n1 = float(linhas[1].strip())
+        n2 = float(linhas[2].strip())
+        faltas = int(linhas[3].strip())
+        resultado = situacaoaluno(n1, n2, faltas)
+        notasTurma.append(resultado[0])
+        situacaoTurma.append(resultado[1])
+        if formato == "html":
+            conteudo.append(f"""<tr>
+                        <td>{nome}</td><td>{n1}</td><td>{n2}</td><td>{faltas}</td><td>{resultado[0]}</td><td>{
+            resultado[1]}</td>
+                        </tr>""")
+        else:
+            conteudo.append(f"""{nome:^30}|{n1:^10}|{n2:^10}|{faltas:^10}|{resultado[0]:^10}|{resultado[1]:^10}\n""")
+
+    estaticaturma = estaticasdaturma()
+    arquivo.close()
+    novo_arquivo = novo_arquivo + "." + formato
+    arquivo = open(novo_arquivo, "w", encoding="UTF-8")
+    if formato == "html":
+        htmlinicio(arquivo)
+        arquivo.writelines(conteudo)
+        htmlfim(arquivo)
+        arquivo.write(f"""<p>Média da turma: {estaticaturma[0]}</br>
+        Quantidade de Alunos acima da média: {estaticaturma[1]}</br>
+        Quantidade de alunos aprovados: {estaticaturma[2]}</br>
+        Quantidade de alunos reprovados: {estaticaturma[3]}</br>
+        Quantidade de alunos em recuperação: {estaticaturma[4]}</br>
+        Quantidade de alunos reprovados por falta: {estaticaturma[5]}</br></p>""")
+    else:
+        arquivo.write(f"""{'NOME':^30}|{'NOTA 1':^10}|{'NOTA 2':^10}|{'FALTAS':^10}|{'MÉDIA':^10}|{'SITUAÇÃO':^10}\n""")
+        arquivo.writelines(conteudo)
+        arquivo.write(f"""\nMédia da turma: {estaticaturma[0]}
+        Quantidade de Alunos acima da média: {estaticaturma[1]}
+        Quantidade de alunos aprovados: {estaticaturma[2]}
+        Quantidade de alunos reprovados: {estaticaturma[3]}
+        Quantidade de alunos em recuperação: {estaticaturma[4]}
+        Quantidade de alunos reprovados por falta: {estaticaturma[5]}""")
+    arquivo.close()
+    print(f"Criado o arquivo {novo_arquivo}.")
+
+
+def htmlinicio(arquivo):
+    arquivo.write(f"""<!DOCTYPE html>
+        <html lang="pt-br">
+          <head>
+            <title>Notas da Turma</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <table border="1">
+            <tr>
+            <th>NOME</th>
+            <th>NOTA 01</th>
+            <th>NOTA 02</th>
+            <th>FALTAS</th>
+            <th>MÉDIA</th>
+            <th>SITUAÇÃO</th>
+            </tr>""")
+
+
+def htmlfim(arquivo):
+    arquivo.write("""</table>
+        </body>
+        </html>""")
+
+
+def situacaoaluno(nota1=0.0, nota2=0.0, faltas=0):
+    media = round((nota1 + nota2)/2, 2)
+    if media < 5:
+        situacao = "Reprovado"
+    elif faltas >= 10:
+        situacao = "Reprovado por falta"
+    elif media < 7:
+        situacao = "Recuperação"
+    else:
+        situacao = "Aprovado"
+    return media, situacao
+
+
+def estaticasdaturma():
+    media = round(sum(notasTurma) / len(notasTurma), 2)
+    qtdacimamedia = 0
+    for n in notasTurma:
+        if n >= media:
+            qtdacimamedia += 1
+    qtdaprovados = situacaoTurma.count('Aprovado')
+    qtdreprovados = situacaoTurma.count('Reprovado')
+    qtdrecuperacao = situacaoTurma.count('Recuperação')
+    qtdreprovfaltas = situacaoTurma.count('Reprovado por falta')
+    return media, qtdacimamedia, qtdaprovados, qtdreprovados, qtdrecuperacao, qtdreprovfaltas
+
+
+def gerarturma(nome):
+    arquivo = open(nome + ".txt", 'w', encoding="UTF-8")
+    for x in range(1, 61):
+        aluno = ("Aluno " + str(x)).title()
+        n1 = round(random() * 10, 2)
+        n2 = round(random() * 10, 2)
+        faltas = randint(1, 15)
+        arquivo.write(f"{aluno}|{n1}|{n2}|{faltas}\n")
+    arquivo.close()
+    print(f"Criado o arquivo {nome}.txt")
+
+
+# analisarturma("teste", 'html', 'base1.txt')
